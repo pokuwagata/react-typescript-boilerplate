2e942e296853630f744704ad7ad980d15f4f7d79
pokuwagata <pokuwagata@gmail.com>
2020-07-12 20:14:15 +0900

    fix

--- webpack.dev.js
+++ webpack.dev.js
@@ -9,16 +9,6 @@ module.exports = merge(common, {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 8080,
-    historyApiFallback: true,
-    proxy: {
-      '/api': {
-        target: 'http://api:5000',
-        pathRewrite: {'^/api' : ''}
-      },
-      '/bff': {
-        target: 'http://bff:4000',
-        pathRewrite: {'^/bff' : ''}
-      }
-    }
+    historyApiFallback: true
   }
 });
